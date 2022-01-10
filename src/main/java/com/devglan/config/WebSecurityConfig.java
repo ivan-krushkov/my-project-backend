01e32bdd219617914340a0ceca4710979676d90a
ivan.krushkov <ivan.krushkov@gps.bg>
2022-01-10 16:05:19 +0200

    add  liveness probe

--- src/main/java/com/devglan/config/WebSecurityConfig.java
+++ src/main/java/com/devglan/config/WebSecurityConfig.java
@@ -48,7 +48,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
   protected void configure(HttpSecurity http) throws Exception {  
     http.cors().and().csrf().disable()
               .authorizeRequests()
-              .antMatchers("/token/*").permitAll()
+              .antMatchers("/token/*", "/actuator/**").permitAll()
               .anyRequest().authenticated()
               .and()
               .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
